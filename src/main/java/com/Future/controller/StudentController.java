dd4c2b0f729f33b80a1895415f1e901590d67b4c
周楠 <906037502@qq.com>
2017-12-10 19:03:52 +0800

    根据前端提交的参数从数据库中从查询

--- src/main/java/com/Future/controller/StudentController.java
+++ src/main/java/com/Future/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,13 @@ public class StudentController {
     public List<Student> getStus(){
         logger.info("从数据库读取Student集合");
         return studentService.getList();
+    }
+
+    @RequestMapping("/student_name")
+    @ResponseBody
+    public List<Student> getStudentInfo(@RequestParam("id") String id) {
+        logger.info("从数据库读取id为："+ id +"的Student集合");
+        return studentService.getStudentInfo(Integer.valueOf(id));
     }
 
 }
--- src/main/java/com/Future/service/StudentService.java
+++ src/main/java/com/Future/service/StudentService.java
@@ -1,7 +1,10 @@
 package com.Future.service;
 
 
+import com.Future.controller.StudentController;
 import com.Future.entity.Student;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
@@ -13,12 +16,32 @@ import java.util.List;
 
 @Service
 public class StudentService {
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
     public List<Student> getList(){
         String sql = "SELECT ID,NAME,SCORE_SUM,SCORE_AVG, AGE FROM student";
+        return (List<Student>) jdbcTemplate.query(sql, new RowMapper<Student>(){
+
+            @Override
+            public Student mapRow(ResultSet rs, int rowNum) throws SQLException {
+                Student stu = new Student();
+                stu.setId(rs.getInt("id"));
+                stu.setAge(rs.getInt("age"));
+                stu.setName(rs.getString("name"));
+                stu.setSumScore(rs.getString("score_sum"));
+                stu.setAvgScore(rs.getString("score_avg"));
+                return stu;
+            }
+
+        });
+    }
+
+    public List<Student> getStudentInfo(int id) {
+        String sql = "select ID,NAME,SCORE_SUM,SCORE_AVG, AGE from student where id = " + id;
+        logger.info("sql查询语句为：" + sql);
         return (List<Student>) jdbcTemplate.query(sql, new RowMapper<Student>(){
 
             @Override
